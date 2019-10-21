fe920344c0568376af281a2bb6c3541c05c2047f
alvase <alvase@gmail.com>
2019-10-21 20:16:38 +1300

    fixed the issues

--- src/StudentList.js
+++ src/StudentList.js
@@ -1,26 +1,22 @@
 import React from "react";
 import BL from "./bookList";
 import { connect } from 'react-redux';
-import {showStudentList} from './bookSystem/systemReducer/actionsCreator';
-import {createBookReturnAction} from './bookSystem/systemReducer/actionsCreator';
+import { showStudentList } from './bookSystem/systemReducer/actionsCreator';
+import { createBookReturnAction } from './bookSystem/systemReducer/actionsCreator';
 
 
 class StudentList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isShowedBookList: false
-    };
   }
-  createStudentOptions = (studentName)=> { 
-      return <option key={studentName} value={studentName}>{studentName}</option>;
-    };
- 
+  createStudentOptions = (studentName) => {
+    return <option key={studentName} value={studentName}>{studentName}</option>;
+  };
+
 
 
   render() {
-    let { isShowedBookList} = this.state;
-    const {selectedStudent,student,isSelected,onBookReturnedRedux,showStudentListRedux}=this.props;
+    const { selectedStudent, student, isSelected, onBookReturnedRedux, showStudentListRedux } = this.props;
 
     return (
       <div>
@@ -28,65 +24,55 @@ class StudentList extends React.Component {
         <label> Choose Student </label>
         <br />
         <select onChange={showStudentListRedux}>
-          {!isSelected&&<option>Please select:</option>}
-          { Object.keys(student).map(this.createStudentOptions)}
+          {!isSelected && <option>Please select:</option>}
+          {Object.keys(student).map(this.createStudentOptions)}
         </select>
         {isSelected && (
           <React.Fragment>
-            <button
-              onClick={() => {
-                this.setState({
-                  isShowedBookList: true
-                });
-              }}
-            >
-              Borrow Book
-            </button>
-            {isShowedBookList && (
-              <React.Fragment>
-               <BL></BL>
-              </React.Fragment>
-            )}
+            <React.Fragment>
+              <BL></BL>
+            </React.Fragment>
             <p> Borrowed Book: </p>
             {/* {Object.keys(student).map(studentName => { */}
-              {/* const books = student[selectedStudent]; */}
-                {/* return ( */}
-                  <React.Fragment>
-                    <span>
-                      {Object.keys(student[selectedStudent]).map((bookName) => {
-                      return <React.Fragment>
-                        {(student[selectedStudent][bookName])&& <p>{selectedStudent + ' borrowed: ' + bookName}
-                         <button
-                      onClick={() => {
-                        onBookReturnedRedux(bookName);
-                          }
+            {/* const books = student[selectedStudent]; */}
+            {/* return ( */}
+            <React.Fragment>
+              <span>
+                {Object.keys(student[selectedStudent]).map((bookName) => {
+                  return <React.Fragment>
+                    {(student[selectedStudent][bookName]) && <p>{selectedStudent + ' borrowed: ' + bookName}
+                      <button
+                        onClick={() => {
+                          onBookReturnedRedux(bookName);
                         }
-                    >
-                      return
+                        }
+                      >
+                        return
                     </button>
                     </p>}
                   </React.Fragment>
-                      })}
-                      </span>
-                      </React.Fragment>
-     </React.Fragment>
-    )}
-    </div>
-    )}
-            }
-const mapStateToProps=(state)=>{
-return{
-  selectedStudent:state.selectedStudent,
-  student: state.student,
-  bookNumbers:state.bookNumbers,
-  isSelected:state.isSelected
-              }
-          }
+                })}
+              </span>
+            </React.Fragment>
+          </React.Fragment>
+        )}
+      </div>
+    )
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    selectedStudent: state.selectedStudent,
+    student: state.student,
+    bookNumbers: state.bookNumbers,
+    isSelected: state.isSelected
+  }
+}
 const mapDispatchToProps = (dispatch) => {
   return {
     showStudentListRedux: (event) => { dispatch(showStudentList(event.target.value)) },
-    onBookReturnedRedux: (bookName)=>{dispatch(createBookReturnAction(bookName))}
-              };
-          };
+    onBookReturnedRedux: (bookName) => { dispatch(createBookReturnAction(bookName)) }
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(StudentList);
-              
+
--- src/bookSystem/systemReducer/reducer.js
+++ src/bookSystem/systemReducer/reducer.js
@@ -1,50 +1,51 @@
-import {SHOW_STUDENT_LIST, RETURN_BOOKS,SUBMIT_BOOKS} from './actionsCreator';
+import { SHOW_STUDENT_LIST, RETURN_BOOKS, SUBMIT_BOOKS } from './actionsCreator';
 const initState = {
-    student: {
-        'Fiona':{},
-        'Frank':{},
-        'Eric':{},
-        'Jane':{}
-      },
-      selectedStudent:null,
-      bookNumbers: {
-        'book1': 5,
-        'book2': 7,
-        'book3': 9,
-        'book4': 3,
-        'book5': 1
-      },
-      isSelected: false,
-      isShowedBookList: false
-    };
+  student: {
+    'Fiona': {},
+    'Frank': {},
+    'Eric': {},
+    'Jane': {}
+  },
+  selectedStudent: null,
+  bookNumbers: {
+    'book1': 5,
+    'book2': 7,
+    'book3': 9,
+    'book4': 3,
+    'book5': 1
+  },
+  isSelected: false,
+  isShowedBookList: false
+};
 
-    export const reducer=(state=initState,action)=>{
-      const { student, selectedStudent, bookNumbers } = state;
-      switch (action.type) {
-        case SHOW_STUDENT_LIST:
-          const studentName = action.studentName
-            return { ...state, selectedStudent: studentName,isSelected:true};
-        case SUBMIT_BOOKS:
-            const tempBookSelect = action.tempBookSelect;
-            Object.keys(tempBookSelect).map((bookName) => {
-              const bookSelected = tempBookSelect[bookName];
-              if (bookSelected) {
-                  if (student[selectedStudent][bookName] === undefined) {
-                      student[selectedStudent][bookName] = false;
-                  }
-                  student[selectedStudent][bookName]=true;
-                  bookNumbers[bookName]--;
-              }
-          });
-            return { student, bookNumbers};
-        case RETURN_BOOKS:
-            const bookName = action.bookName;
-            student[selectedStudent][bookName]=false;
-            bookNumbers[bookName]++;
-            return{
-              student, bookNumbers
-            };
-        default:
-            return state;
+export const reducer = (state = initState, action) => {
+  const { student, selectedStudent, bookNumbers } = state;
+  switch (action.type) {
+    case SHOW_STUDENT_LIST:
+      const studentName = action.studentName
+      return { ...state, selectedStudent: studentName, isSelected: true };
+    case SUBMIT_BOOKS:
+      const tempBookSelect = action.tempBookSelect;
+      Object.keys(tempBookSelect).map((bookName) => {
+        const bookSelected = tempBookSelect[bookName];
+        if (bookSelected) {
+          student[selectedStudent] = { ...student[selectedStudent] };
+          if (student[selectedStudent][bookName] === undefined) {
+            student[selectedStudent][bookName] = false;
           }
+          student[selectedStudent][bookName] = true;
+          bookNumbers[bookName]--;
         }
+      });
+      return { ...state, student: { ...student, updated: true }, bookNumbers };
+    case RETURN_BOOKS:
+      const bookName = action.bookName;
+      student[selectedStudent][bookName] = false;
+      bookNumbers[bookName]++;
+      return {
+        ...state, student: { ...student, updated: true }, bookNumbers
+      };
+    default:
+      return state;
+  }
+}
